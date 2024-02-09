c21502d21e800505bdd09c2d029cc2af82d43ea1
nandu44-code <nanduganeshnair4444@gmail.com>
2024-02-09 22:57:52 +0530

    User model created

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,50 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    
+    profile_image_url = models.URLField(blank=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=20, unique=False, verbose_name='username',
+                             blank=True,null=True)
+    otp = models.IntegerField(default=0)
+    is_verified = models.BooleanField(default=False)
+    
+
+    USERNAME_FIELD = 'email'
+    
+
+    objects = CustomUserManager()
+
+    
+    class Meta:
+        verbose_name='CustomUser'
+        verbose_name_plural="CustomUsers"
+
+
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_set', blank=True)
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_set', blank=True)
