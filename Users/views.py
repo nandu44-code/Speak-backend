9f022043205bc4bfd7e82c180072595be175beae
nandu44-code <nanduganeshnair4444@gmail.com>
2024-02-11 00:41:43 +0530

    UserRegistationSerializer is added in serializer

--- Users/views.py
+++ Users/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from django.contrib.auth.models import User
+from .models import CustomUser
+from .serializers import UserRegistrationSerializer
 
+# views.py
+class UserRegistrationViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.none()  # This is just a placeholder queryset since we are not using a queryset for registration
+    serializer_class = UserRegistrationSerializer
