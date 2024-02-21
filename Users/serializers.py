48800d0eda43f5f14a28816eef68fd5275b72a4c
nandu44-code <nanduganeshnair4444@gmail.com>
2024-02-21 20:41:52 +0530

    User serializer updated with create function

--- Users/serializers.py
+++ Users/serializers.py
@@ -8,6 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
