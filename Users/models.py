1e75361fa2b911741532bad6a38980634fd031ae
nandu44-code <nanduganeshnair4444@gmail.com>
2024-03-15 03:14:29 +0530

    Created model slot

--- Users/models.py
+++ Users/models.py
@@ -66,3 +66,8 @@ class Tutor(models.Model):
     is_approved = models.BooleanField(default=False)
     
 
+class Slot(models.Model):
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
