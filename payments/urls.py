a84d31500022d974edeb2cbf4e8915e042aef4c5
nandu44-code <nanduganeshnair4444@gmail.com>
2024-03-29 00:32:26 +0530

    create checkout session view created

--- payments/urls.py
+++ payments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from payments import views
 
 urlpatterns =[
-   path('test-payment/', views.test_payment)
+   path('test-payment/', views.test_payment),
+   path('create-checkout-session/', views.create_checkout_session, name='create-checkout-session'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -13,7 +13,33 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 def test_payment(request):
     print('heiii')
     test_payment_intent = stripe.PaymentIntent.create(
-    amount=1000, currency='pln', 
+    amount=200000000, currency='inr', 
     payment_method_types=['card'],
     receipt_email='test@example.com')
     return Response(status=status.HTTP_200_OK, data=test_payment_intent)
+
+@api_view(['POST'])
+def create_checkout_session(request):
+    if request.method == 'POST':
+        try:
+            print("create checkout session.......")
+            # Create a Checkout Session
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[{
+                    'price_data': {
+                        'currency': 'inr',
+                        'product_data': {
+                            'name': 'Slot Booking',
+                        },
+                        'unit_amount': 250000,
+                    },
+                    'quantity': 1,
+                }],
+                mode='payment',
+                success_url='http://localhost:5173',
+                cancel_url='http://localhost:5173/student/profile',
+            )
+            return Response({'id': checkout_session.id})
+        except Exception as e:
+            return Response({'error': str(e)}, status=400)
