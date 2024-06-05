c677dc3e9e2fd81921cb3d506ce4949813cd3645
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-05 15:33:15 +0530

    Configured asgi

--- Speak/asgi.py
+++ Speak/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Speak.settings')
 
 application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            [
+            #  path("ws/chat/<str:room_name>/", ChatConsumer.as_asgi()),
+            ]
+        )
+    ),
+})
