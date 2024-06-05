933364ad0c773410ad2306f6d5d3cefa635d9171
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-05 15:07:12 +0530

    Created Message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from user.models import CustomUser
 # Create your models here.
+class Message(models.Model):
+
+    sender = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+    receiver = models.Foreignkey(CustomUser, on_delete = models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"From: {self.sender.first_name} To: {self.receiver.first_name}"
