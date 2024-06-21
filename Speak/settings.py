cef82bf15df7a86260ddcf3227a85b0355ba8eab
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-21 21:09:23 +0530

    static files updated

--- Speak/settings.py
+++ Speak/settings.py
@@ -144,22 +144,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-# STATIC_URL = '/static/'
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media',
-
-
-STATIC_URL = 'static/'
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-    STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-else:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-    STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media',
+
+
+# STATIC_URL = 'static/'
+# if DEBUG:
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+# else:
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
